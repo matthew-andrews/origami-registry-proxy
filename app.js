db622f30eb26961aaaaba5dd08ba96e8f7c7ccfc
Matthew Andrews <matt@mattandre.ws>
2015-01-30 23:22:18 +0000

    Handle 404s, 500s

--- app.js
+++ app.js
@@ -9,21 +9,25 @@ app.use(function(req, res) {
 	fetch('http://registry.origami.ft.com'+req.path)
 		.then(function(response) {
 			res.status(response.status);
-			return response.json();
-		})
-		.then(function(data) {
-			if (Array.isArray(data)) {
-				data = data.map(function(pkg) {
-					if (pkg.url) {
-						pkg.url = pkg.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
-					}
-					return pkg;
+			if (!res.ok) {
+				return response.text().then(function(err) {
+					res.send(err);
 				});
-				console.log(data);
-			} else if (data.url) {
-				data.url = data.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
 			}
-			res.json(data);
+			return response.json()
+				.then(function(data) {
+					if (Array.isArray(data)) {
+						data = data.map(function(pkg) {
+							if (pkg.url) {
+								pkg.url = pkg.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
+							}
+							return pkg;
+						});
+					} else if (data.url) {
+						data.url = data.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
+					}
+					res.json(data);
+				});
 		})
 		.catch(function(err) {
 			console.log(err);
