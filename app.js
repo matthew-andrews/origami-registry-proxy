972926746827486f019712d8a46a8c62266920c3
Matthew Andrews <matt@mattandre.ws>
2015-01-30 23:09:35 +0000

    Correct fonts

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var express = require('express');
+require('es6-promise').polyfill();
+var fetch = require('node-fetch');
+
+var app = express();
+app.use(function(req, res) {
+	fetch('http://registry.origami.ft.com'+req.path)
+		.then(function(response) {
+			res.status(response.status);
+			return response.json();
+		})
+		.then(function(data) {
+			if (Array.isArray(data)) {
+				data = data.map(function(pkg) {
+					if (pkg.url) {
+						pkg.url = pkg.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
+					}
+					return pkg;
+				});
+				console.log(data);
+			} else if (data.url) {
+				data.url = data.url.replace("http://git.svc.ft.com/scm/rip/o-fonts.git", "https://github.com/Financial-Times/o-fonts.git");
+			}
+			res.json(data);
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+
+});
+app.listen(process.env.PORT || 5050);
